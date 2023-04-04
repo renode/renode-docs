a509607730f2a0ff689e8ae666fc2e9cb82a1d83
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2023-04-04 09:44:48 +0200

    Add Cortex-A53 to supported platforms

--- tools/peripherals_scanner.py
+++ tools/peripherals_scanner.py
@@ -57,6 +57,7 @@ def try_get_category(platform):
         "cc2538":  "TI",
         "colibri-vf61":    "NXP I.MX",
         "core-v-mcu":  "OHG",
+        "cortex-a53": "GENERIC ARM",
         "crosslink-nx-evn":    "LITEX",
         "efm32g210":   "EFM",
         "efm32g222":   "EFM",
