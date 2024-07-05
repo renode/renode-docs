2c0a85b85f82bf7603b76982b4239b48bf9de4f9
Piotr Wojnarowski <pwojnarowski@antmicro.com>
2024-07-05 14:46:48 +0200

    [#62020] Get Renode with submodules with one git clone command

--- generate-renode-platforms.sh
+++ generate-renode-platforms.sh
@@ -4,9 +4,8 @@ mkdir -p build/html/introduction
 
 cd build
 if [ ! -d renode ]; then
-    git clone --depth=1 --shallow-submodules https://github.com/renode/renode
+    git clone --depth=1 --recursive --shallow-submodules https://github.com/renode/renode
 fi
-(cd renode && git submodule update --init --recursive)
 cd ..
 
 python3 tools/peripherals_scanner.py --dir build/renode -H > source/introduction/renode_supported_peripherals.html
